4ce89606c0a17cb7e66ea268f7d4200f977deb83
Dagonin <43449141+Dagonin@users.noreply.github.com>
2024-01-25 08:53:07 +0100

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from tkinter import messagebox
 import paho.mqtt.client as mqtt
 import tkinter
+from bankomat import *
 
 # The terminal ID - can be any string.
-terminal_id = "T0"
+terminal_id = "T1"
 # The broker name or IP address.
 broker = "localhost"
 # broker = "127.0.0.1"
@@ -16,38 +18,63 @@ client = mqtt.Client()
 # Thw main window with buttons to simulate the RFID card usage.
 window = tkinter.Tk()
 
-def call_worker(worker_name):
-    client.publish("worker/name", worker_name + "." + terminal_id,)
+def call_action(action,argument):
+    client.publish("worker/name", action + ","+ str(argument) +"," + terminal_id,)
+
+def call_worker(action):
+    client.publish("worker/name", action +"," + terminal_id,)
+
 
 
 def create_main_window():
-    window.geometry("300x200")
+    window.geometry("400x200")
     window.title("SENDER")
 
-    intro_label = tkinter.Label(window, text="Select employee:")
-    intro_label.grid(row=0, columnspan=5)
+    intro_label = tkinter.Label(window, text="Bankomat")
+    intro_label.grid(row=0, column=0, columnspan=2)
 
-    button_1 = tkinter.Button(window, text="Employee 1",
-                              command=lambda: call_worker("Employee 1"))
-    button_1.grid(row=1, column=0)
-    button_2 = tkinter.Button(window, text="Employee 2",
-                              command=lambda: call_worker("Employee 2"))
-    button_2.grid(row=2, column=0)
-    button_3 = tkinter.Button(window, text="Employee 3",
-                              command=lambda: call_worker("Employee 3"))
-    button_3.grid(row=3, column=0)
-    button_4 = tkinter.Button(window, text="Employee 4",
-                              command=lambda: call_worker("Employee 4"))
-    button_4.grid(row=1, column=1)
-    button_5 = tkinter.Button(window, text="Employee 5",
-                              command=lambda: call_worker("Employee 5"))
-    button_5.grid(row=2, column=1)
-    button_6 = tkinter.Button(window, text="Employee 6",
-                              command=lambda: call_worker("Employee 6"))
-    button_6.grid(row=3, column=1)
+
+    entry1 = create_entry(window)
+    button1 = tkinter.Button(window, text="Login", command=lambda: call_action("login",get_int(entry1)))
+    button1.grid(row=1, column=1, sticky="e")
+    button11 = tkinter.Button(window, text="Bad PIN", command=lambda: call_action("input_pin","False"))
+    button11.grid(row=1, column=2, sticky="e")
+
+    button2 = tkinter.Button(window, text="Check Balance", command=lambda:call_action("check_balance",""))
+    button2.grid(row=2, column=1, sticky="e")
+
+    entry3 = create_entry(window)
+    button3 = tkinter.Button(window, text="Deposit", command=lambda: call_action("deposit",get_float(entry3)))
+    button3.grid(row=3, column=1, sticky="e")
+
+    entry4 = create_entry(window)
+    button4 = tkinter.Button(window, text="Withdraw", command=lambda: call_action("withdraw",get_float(entry4)))
+    button4.grid(row=4, column=1, sticky="e")
+
+    button5 = tkinter.Button(window, text="Logout", command=lambda: call_action("logout",""))
+    button5.grid(row=5, column=1, sticky="e")
+
     button_stop = tkinter.Button(window, text="Stop", command=window.quit)
-    button_stop.grid(row=4, columnspan=2)
+    button_stop.grid(row=7, columnspan=2)
 
+def create_entry(parent):
+    entry = tkinter.Entry(parent)
+    entry.grid(sticky="w")
+    return entry
+
+def get_int(entry):
+    try:
+        return int(entry.get())
+    except ValueError:
+        messagebox.showerror("Error", "Please enter a valid int.")
+        return 0
+
+def get_float(entry):
+    try:
+        return float(entry.get())
+    except ValueError:
+        messagebox.showerror("Error", "Please enter a valid float.")
+        return 0
 
 def connect_to_broker():
     # Connect to the broker.
